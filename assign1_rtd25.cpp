5f8e793548435c895eef8ab80e6779e21b913d0a
Rex Davis <davisrex78704@gmail.com>
2018-01-23 17:25:37 -0600

    Project1F
    Finished Program

---
+++ assign1_rtd25.cpp
@@ -0,0 +1,26 @@
+// File Name: assign1_rtd25.cpp
+//
+// Author: Rex Davis
+// Date: 1/23/2018
+// Assignment Number: 1
+// CS 1428.302 Spring 2018
+// Instructor: Jill Seaman
+//
+// (Description of what the program does goes here)
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+// statements that do stuff
+	cout << "Name: Rex Davis" << endl;
+	cout << "Major: Electrical Engineering" << endl;
+	cout << "Favorite Song: \"Travelin' Band\" By Creedence Clearwater Revival" << endl;
+
+
+// Exit the program
+	return 0;
+
+}
